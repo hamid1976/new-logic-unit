0ca25a12c338492fb0378ca674729cd93ccafbc8
logicunit01-star <logicunit01@gmail.com>
2026-06-15 18:05:34 +0500

    fix: programmatically strip www from siteUrl

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,6 +1,5 @@
-export const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, '') ||
-  'https://logic-unit.com'
+const rawUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://logic-unit.com'
+export const siteUrl = rawUrl.replace(/\/$/, '').replace('://www.', '://')
 
 export const logoPath = '/logic-unit-logo.png'
 export const logoUrl = `${siteUrl}${logoPath}`
